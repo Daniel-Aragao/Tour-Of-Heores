764f2504676067b302d1c6373dc4d11fca790421
Daniel-Aragao <dan.dan.44@hotmail.com>
2016-12-06 21:04:36 -0300

    first

---
+++ quickstart/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { HeroesComponent } from './heroes.component';
+import { HeroService } from './hero.service';
+
+@Component({
+    selector: 'my-app',
+    providers: [HeroService],
+    template: `
+    <h1>{{title}}</h1>
+    <nav>
+        <a routerLink="/dashboard" routerLinkActive="active">Dashboard</a>
+        <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+    `,
+    styleUrls: ['./app/app.component.css']
+})
+
+export class AppComponent{
+    title = 'Tour of Heroes';
+
+}
